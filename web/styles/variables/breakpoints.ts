ff08d1f5f7f03732e02bd39684fa8c6ef91352f9
Rastrent <107659432+rastrent@users.noreply.github.com>
2022-07-18 16:27:54 -0400

    add some base style variables

---
+++ web/styles/variables/breakpoints.ts
@@ -0,0 +1,18 @@
+/**
+ * Breakpoint variables to define min-width screen sizes.
+ */
+
+// Phone devices (landscape phones)
+export const phone = "min-width: 576px";
+
+// Tablet devices (tablets)
+export const tablet = "min-width: 768px";
+
+// Desktop devices (desktops)
+export const desktop = "min-width: 992px";
+
+// Large desktop devices (large desktops)
+export const desktopLarge = "min-width: 1200px";
+
+// Extra large desktop devices (xlarge desktops)
+export const desktopXL = "min-width: 1200px";
---
+++ web/styles/variables/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Export style variables.
+ */
+
+import * as breakpoints from "./breakpoints";
+import * as colors from "./colors";
+import * as spacing from "./spacing";
+import * as typography from "./typography";
+
+export { breakpoints, colors, spacing, typography };
