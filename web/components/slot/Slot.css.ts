89a4d8a6d38f58f1b3683ffc3c653f07682e908c
Rastrent <107659432+rastrent@users.noreply.github.com>
2022-08-13 16:45:34 -0400

    adding emotion for styles

---
+++ web/components/slot/Slot.css.ts
@@ -0,0 +1,7 @@
+import { css } from "@emotion/react";
+
+export const Slot = () => css`
+  * {
+    font-family: "Roboto", sans-serif !important;
+  }
+`;
---
+++ web/styles/home.css.ts
@@ -0,0 +1,5 @@
+import { css } from "@emotion/react";
+
+export const homeBaseStyle = () => css`
+  background-color: #000 !important;
+`;
